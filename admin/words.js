30b465e296f23620009a36f0a7a6d0e79407a79e
Slugger2k <christian.muell3r@gmail.com>
2026-04-09 17:44:00 +0200

    fix config

---
+++ admin/words.js
@@ -0,0 +1,26 @@
+systemDictionary = {
+    "marstek-venus settings": {
+        "en": "Marstek Venus settings",
+        "de": "Marstek Venus Einstellungen"
+    },
+    "ipAddress": {
+        "en": "Device IP Address",
+        "de": "Geräte IP Adresse"
+    },
+    "udpPort": {
+        "en": "UDP Port",
+        "de": "UDP Port"
+    },
+    "pollInterval": {
+        "en": "Poll interval (ms)",
+        "de": "Abfrageintervall (ms)"
+    },
+    "autoDiscovery": {
+        "en": "Auto discover devices",
+        "de": "Geräte automatisch erkennen"
+    },
+    "discover": {
+        "en": "Discover devices",
+        "de": "Geräte suchen"
+    }
+};
